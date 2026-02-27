977585f7e5a3a6a2590a15c19f035b71e0cc8c6b
JoaoVitorSzCandido <joaobonitao764@gmail.com>
2026-02-27 00:42:02 -0300

    fiz um sistema de verificação na lista

--- sandbox/d/assets/js/main.js
+++ sandbox/d/assets/js/main.js
@@ -1,21 +1,33 @@
 const button = document.querySelector("#save");
 const listFood = document.querySelector("#foodList");
 let itemText = document.querySelector("#item");
-
+let textStorage = [];
 
 button.addEventListener("click", function(){
   let newItem = document.createElement("li");
   let removeItem = document.createElement("button");
   
-  let text = itemText.value
+  let text = itemText.value;
   
-  newItem.textContent = text
-  removeItem.textContent = "Remove"
+  newItem.textContent = text;
+  removeItem.textContent = "Remove";
   removeItem.addEventListener("click", function(){
     newItem.remove()}
   )
   
+  if (text === ""){
+    alert("Empty field")
+  }else if (textStorage.includes(text)) {
+    alert("Already exists")
+  }else {
+    
+  textStorage.push(text)
+  
   newItem.appendChild(removeItem)
   listFood.appendChild(newItem)
+  };
+  
+  console.log(textStorage);
+  
 }
 )
